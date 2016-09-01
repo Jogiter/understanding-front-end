8ea973d925bf8e6d4496d152624c2076f55caa83
Jogiter <jogiter.g@gmail.com>
2016-09-01 16:08:15 +0800

    update gulp flow

--- others/gulp-flow/gulpfile.js
+++ others/gulp-flow/gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 	pump = require('pump'),
 	uglify = require('gulp-uglify'),
 	del = require('del'),
+	qiniu = require('gulp-qiniu'),
 	RevAll = require('gulp-rev-all');
 
 var config = {
@@ -12,13 +13,23 @@ var config = {
 	dist: './static/',
 	manifest: './',
 	cdn: {
-		ak: '',
-		sk: ''
+		url: 'http://oasvenh8x.bkt.clouddn.com/static/',
+		base: {
+			accessKey: "Pv4kMjn_1gXEvP2Efk9zEB-C2gRWAC-LLF_DsZ2Z",
+			secretKey: "dEGaG6VgDxFHSNTFjvU6MaSkXRcILKv-Pb-KfvZo",
+			bucket: "jogiter",
+			private: false
+		},
+		dist: {
+			dir: 'static/',
+			versioning: false, // false则后面的值不生效
+			versionFile: './cdn.json',
+			concurrent: 10 // 时间值
+		}
 	}
 };
 
 gulp.task('min:js', ['rev'], function() {
-	var revAll = new RevAll();
 	pump([gulp.src(config.dist + 'src/js/**/*.js'), uglify(), gulp.dest(config.dist + 'src/js/')], function() {
 		console.log('======uglify done======');
 	});
@@ -27,7 +38,7 @@ gulp.task('min:js', ['rev'], function() {
 gulp.task('min:css', ['rev'], function() {
 	return gulp.src(config.dist + 'src/css/**/*.css')
 		.pipe(cleanCss({
-			compatibility: '*'
+			compatibility: '*' // default: ie9+
 		}, function() {
 			console.log('======cleanCss done======');
 		}))
@@ -38,11 +49,14 @@ gulp.task('min', ['min:js', 'min:css']);
 
 gulp.task('rev', ['clear'], function() {
 	var revAll = new RevAll({
+		transformPath: function(rev, source, path) {
+			// replace you source with cdn url
+			return rev.replace(/(.*src\/)/ig, config.cdn.url);
+		},
 		dontRenameFile: [/.html$/g],
 		dontUpdateReference: [/.html$/g],
 	});
 	return gulp.src([config.src + '**/*', config.app + '**/*.html'])
-		// .pipe(gulp.dest(config.dist))
 		.pipe(revAll.revision())
 		.pipe(gulp.dest(config.dist))
 		.pipe(revAll.manifestFile())
@@ -50,7 +64,12 @@ gulp.task('rev', ['clear'], function() {
 });
 
 gulp.task('clear', function() {
-	del.sync([config.dist, './rev-manifest.json']);
+	del.sync([config.dist, './rev-manifest.json', './cdn.json']);
 });
 
-gulp.task('default', ['min']);
+gulp.task('cdn', ['min'], function() {
+	return gulp.src(config.dist + 'src/**')
+		.pipe(qiniu(config.cdn.base, config.cdn.dist));
+});
+
+gulp.task('default', ['min', 'cdn']);
