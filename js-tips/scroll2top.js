068c56596d37021a46afc038f25a6c85aa1a8761
Jogiter <jogiter.g@gmail.com>
2017-04-27 12:16:03 +0800

    add raf scroll2top

---
+++ js-tips/scroll2top.js
@@ -0,0 +1,47 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||    // Webkit中此取消方法的名字变了
+                                      window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+            var id = window.setTimeout(function() {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+}());
+
+function scroll(pos, time) {
+    pos = pos || 0;
+    time = time || 1000;
+    var i = 0,
+        raf,
+        scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop,
+        speed = (pos - scrollTop) / 60 / time * 1000;
+    function _scroll() {
+        var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+        window.scrollTo(0, scrollTop += speed)
+        if (scrollTop <= pos) {
+            window.cancelAnimationFrame(raf)
+        } else {
+            raf = window.requestAnimationFrame(_scroll);
+        }
+    }
+    window.requestAnimationFrame(_scroll);
+}
+
+module.exports = scroll;
