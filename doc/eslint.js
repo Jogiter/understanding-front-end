a0fecb42d052c334f0e0f27f02a7f491de1fa648
Jogiter <jogiter.g@gmail.com>
2017-07-26 17:42:20 +0800

    add eslint

---
+++ doc/eslint.js
@@ -0,0 +1,13 @@
+## [eslint](http://eslint.org/)
+
+## install
+
+>$ npm install -g eslint
+>$ eslint --init
+>$ eslint yourfile.js
+
+
+## links
+
++	[eslint:recommended](http://eslint.org/docs/rules/)
++	[standard](https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md)
