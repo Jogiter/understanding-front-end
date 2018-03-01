31092a76cd091c86602c6bdcf627e636ac86d5a6
Jogiter <jogiter.g@gmail.com>
2018-03-01 13:49:03 +0800

    fix .config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,20 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'no-console': ["error", {
-        allow: ['warn', 'error', 'log']
-    }]
+  extends: "standard",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    // indent: ["error", 2],
+    quotes: ["off", "single"],
+    // semi: ["error", "always"]
+  },
+  globals: {
   }
-}
+};
