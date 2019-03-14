f8e7d72efe7c154dbd9747cbd70432fa66f4fd22
Jogiter <Jogiter@users.noreply.github.com>
2019-03-14 18:38:16 +0800

    Update fibonacci.js

--- js-tips/fibonacci.js
+++ js-tips/fibonacci.js
@@ -1,4 +1,5 @@
 /**
+ * [ECMAScript 6 入门](http://es6.ruanyifeng.com/#docs/function#尾递归优化的实现)
  * 函数调用自身，称为递归。如果尾调用自身，就称为尾递归。
  * 递归非常耗费内存，但对于尾递归来说，由于只存在一个调用帧，所以永远不会发生“栈溢出”错误。
  * 不同方式实现菲波那切数列
@@ -14,6 +15,8 @@ function Fibonacci (n) {
   }
   return Fibonacci(n - 2) + Fibonacci(n - 1)
 }
+// Fibonacci(100000) 
+// Uncaught RangeError: Maximum call stack size exceeded
 
 // 尾递归。只保留一个调用记录，复杂度 O(1)
 function Fibonacci (n, ac1 = 1, ac2 = 1) {
@@ -22,4 +25,37 @@ function Fibonacci (n, ac1 = 1, ac2 = 1) {
   }
   return Fibonacci(n - 1, ac2, ac1 + ac2)
 }
+// Fibonacci(100000) 
+// Uncaught RangeError: Maximum call stack size exceeded
+
+
+// 尾递归优化
+function tco(f) {
+  var value;
+  var active = false;
+  var accumulated = [];
+
+  return function accumulator() {
+    accumulated.push(arguments);
+    if (!active) {
+      active = true;
+      while (accumulated.length) {
+        value = f.apply(this, accumulated.shift());
+      }
+      active = false;
+      return value;
+    }
+  };
+}
+
+let fib = tco(function(n, ac1 = 1, ac2 = 1) {
+  if (n <= 1) {
+    return ac2
+  }
+  return fib(n - 1, ac2, ac1 + ac2)
+})
+
+// 不会发生调用栈溢出
+let res = fib(100000)
+console.log(res) // Infinity
 
